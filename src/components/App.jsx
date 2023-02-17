c56b421bca434cf78418600b5f5f0720364034c8
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-02-17 12:44:42 +0100

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { UserList } from './UserList';
+import user from 'user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <UserList items={user} /> 
     </div>
   );
 };
---
+++ src/components/UserList.js
@@ -0,0 +1,43 @@
+
+export const Profile = ({
+    items: {
+        avatar,
+        username,
+        tag,
+        location,
+        stats: {
+            followers,
+            views,
+            likes,
+        },
+    }
+}) => {
+    return (
+      <div>
+        <div>
+          <img src={avatar} alt="User avatar" />
+          <p>{username}</p>
+          <p>{tag}</p>
+          <p>{location}</p>
+        </div>
+
+        <ul>
+          <li>
+            <span class="label">Followers:</span>
+            <span class="quantity">{followers}</span>
+          </li>
+          <li>
+            {' '}
+            <span class="label">Views</span>
+            <span class="quantity">{views}</span>
+          </li>
+          <li>
+            <span class="label">Likes</span>
+            <span class="quantity">{likes}</span>
+          </li>
+        </ul>
+      </div>
+    );
+};
+
+Profile.propTypes = {}
